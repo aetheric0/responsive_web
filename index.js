660e23b144fa3717a3b570d41b1f27209f3b4591
Monarch <125897342+aetheric0@users.noreply.github.com>
2024-03-18 05:48:27 +0300

    responsive web design

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.getElementById("count-el").innerHTML = "Dynamic"
+
+// let count = 5
+// let myAge = 1001
+
+// console.log(count)
+// console.log(myAge)
+
+myAge = 19
+humanDogRatio = 7
+
+myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
